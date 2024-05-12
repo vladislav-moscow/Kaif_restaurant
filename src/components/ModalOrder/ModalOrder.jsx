e0b0ac25713a7bfdc89707f4c68799cac879cd14
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-05-12 16:31:19 +0300

    сделал отправку данных бронирования столика

--- src/components/ModalOrder/ModalOrder.jsx
+++ src/components/ModalOrder/ModalOrder.jsx
@@ -1,12 +1,24 @@
 'use client';
-import { useCallback, useEffect, useRef, useState } from 'react';
-import './modalOrder.css';
+import { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
+import { nanoid } from 'nanoid';
+
 import { formatDate } from '@/utils';
+import TimeOption from '../TimeOption/TimeOption';
+
+import './modalOrder.css';
+
 
 const ModalOrder = ({ open, modalRef, onClose }) => {
-	const [personCount, setPersonCount] = useState(2);
-	const [selectedDate, setSelectedDate] = useState(''); // Состояние для хранения выбранной даты
-	const [dates, setDates] = useState([]); // Состояние для хранения списка дат
+	const [dates, setDates] = useState([]);// Состояние для хранения списка дат
+	const [selectedTime, setSelectedTime] = useState(''); // выбранное время
+	const [formData, setFormData] = useState({
+		id: '',
+		date: '',
+		personCount: 2,
+		сomment: '',
+		gap: '',
+	}); // объект формы
 
 	useEffect(() => {
 		const handleClickOutside = (event) => {
@@ -39,25 +51,63 @@ const ModalOrder = ({ open, modalRef, onClose }) => {
 			}
 			return dateOptions;
 		};
-
 		// Устанавливаем список дат в состояние
 		setDates(generateDates());
 	}, []);
 
-	// Функция для обработки изменения выбранной даты
-	const handleDateChange = useCallback((event) => {
-		setSelectedDate(event.target.value);
+	const handleChangeFormData = useCallback((event) => {
+		setFormData((prev) => {
+			return {
+				...prev,
+				[event.target.name]: event.target.value,
+			};
+		});
+		setSelectedTime(event.target.value);
 	}, []);
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		// Добавляем id к объекту formData
+		const formDataWithId = {
+			...formData,
+			id: nanoid(),
+		};
+		try {
+				const response = await axios.post('http://localhost:3001/tableReservation', formDataWithId);
+				console.log(response.data);
+				// Обработка успешного ответа от сервера
+
+				//сброс данных
+
+				setFormData({
+					id: '',
+					date: '',
+					personCount: 2,
+					сomment: '',
+					gap: '',
+			});
+		} catch (error) {
+				console.error('Error submitting form:', error);
+				// Обработка ошибки при отправке данных на сервер
+		}
+	};
+
 	const handleDecrease = () => {
-		if (personCount > 1) {
-			setPersonCount(personCount - 1);
+		if (formData.personCount > 1) {
+			setFormData((prevState) => ({
+				...prevState,
+				personCount: prevState.personCount - 1,
+			}));
 		}
 	};
 
 	const handleIncrease = () => {
-		if (personCount < 6) {
-			setPersonCount(personCount + 1);
+		if (formData.personCount < 6) {
+			setFormData((prevState) => ({
+				...prevState,
+				personCount: prevState.personCount + 1,
+			}));
 		}
 	};
 
@@ -73,25 +123,29 @@ const ModalOrder = ({ open, modalRef, onClose }) => {
 					Х
 				</button>
 			</header>
-			<form action='' className='modal-form'>
+			<form
+				onSubmit={handleSubmit}
+				action=''
+				className='modal-form'
+				id='reservationForm'
+			>
 				<div className='form-date__wrapper flex'>
 					<label htmlFor='date'>
 						<h4 className='form-date__title'>Дата брони</h4>
-						<selectmenu>
 							<select
-								value={selectedDate}
-								onChange={handleDateChange}
-								name='Date'
+								value={formData.date}
+								onChange={handleChangeFormData}
+								name='date'
 								className='form-date__select'
 								id='date'
 							>
+								<option value=''>Выберите дату</option>
 								{dates.map((date, index) => (
 									<option key={index} value={date}>
 										{date}
 									</option>
 								))}
 							</select>
-						</selectmenu>
 					</label>
 					<label htmlFor='counter' className='counter-visitors'>
 						<h4 className='form-date__title'>Кол-во человек</h4>
@@ -106,7 +160,8 @@ const ModalOrder = ({ open, modalRef, onClose }) => {
 							<input
 								id='counter'
 								type='number'
-								value={personCount}
+								value={formData.personCount}
+								onChange={handleChangeFormData}
 								autoComplete='new-password'
 								maxLength={2}
 								readOnly
@@ -127,300 +182,183 @@ const ModalOrder = ({ open, modalRef, onClose }) => {
 						<h4 className='form-date__title'>Выбор времени</h4>
 					</label>
 					<div id='gaps' role='group'>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap720'
-								name='gap'
-								value={720}
-								itemProp='0'
-							/>
-							<label htmlFor='gap720' className='time-gap-label' value={720}>
-								<span className='gap-text'> 12:00 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap750'
-								name='gap'
-								value={750}
-								itemProp='0'
-							/>
-							<label htmlFor='gap750' className='time-gap-label' value={750}>
-								<span className='gap-text'> 12:30 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap780'
-								name='gap'
-								value={780}
-								itemProp='0'
-							/>
-							<label htmlFor='gap780' className='time-gap-label' value={780}>
-								<span className='gap-text'> 13:00 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap810'
-								name='gap'
-								value={810}
-								itemProp='0'
-							/>
-							<label htmlFor='gap810' className='time-gap-label' value={810}>
-								<span className='gap-text'> 13:30 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap840'
-								name='gap'
-								value={840}
-								itemProp='0'
-							/>
-							<label htmlFor='gap840' className='time-gap-label' value={840}>
-								<span className='gap-text'> 14:00 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap870'
-								name='gap'
-								value={870}
-								itemProp='0'
-							/>
-							<label htmlFor='gap870' className='time-gap-label' value={870}>
-								<span className='gap-text'> 14:30 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap900'
-								name='gap'
-								value={900}
-								itemProp='0'
-							/>
-							<label htmlFor='gap900' className='time-gap-label' value={900}>
-								<span className='gap-text'> 15:00 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap930'
-								name='gap'
-								value={930}
-								itemProp='0'
-							/>
-							<label htmlFor='gap930' className='time-gap-label' value={930}>
-								<span className='gap-text'> 15:30 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap960'
-								name='gap'
-								value={960}
-								itemProp='0'
-							/>
-							<label htmlFor='gap960' className='time-gap-label' value={960}>
-								<span className='gap-text'> 16:00 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap990'
-								name='gap'
-								value={990}
-								itemProp='0'
-							/>
-							<label htmlFor='gap990' className='time-gap-label' value={990}>
-								<span className='gap-text'> 16:30 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap1020'
-								name='gap'
-								value={1020}
-								itemProp='0'
-							/>
-							<label htmlFor='gap1020' className='time-gap-label' value={1020}>
-								<span className='gap-text'> 17:00 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap1050'
-								name='gap'
-								value={1050}
-								itemProp='0'
-							/>
-							<label htmlFor='gap1050' className='time-gap-label' value={1050}>
-								<span className='gap-text'> 17:30 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap1080'
-								name='gap'
-								value={1080}
-								itemProp='0'
-							/>
-							<label htmlFor='gap1080' className='time-gap-label' value={1080}>
-								<span className='gap-text'> 18:00 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap1110'
-								name='gap'
-								value={1110}
-								itemProp='0'
-							/>
-							<label htmlFor='gap1110' className='time-gap-label' value={1110}>
-								<span className='gap-text'> 18:30 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap1140'
-								name='gap'
-								value={1140}
-								itemProp='0'
-							/>
-							<label htmlFor='gap1140' className='time-gap-label' value={1140}>
-								<span className='gap-text'> 19:00 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap1170'
-								name='gap'
-								value={1170}
-								itemProp='0'
-							/>
-							<label htmlFor='gap1170' className='time-gap-label' value={1170}>
-								<span className='gap-text'> 19:30 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap1200'
-								name='gap'
-								value={1200}
-								itemProp='0'
-							/>
-							<label htmlFor='gap1200' className='time-gap-label' value={1200}>
-								<span className='gap-text'> 20:00 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap1230'
-								name='gap'
-								value={1230}
-								itemProp='0'
-							/>
-							<label htmlFor='gap1230' className='time-gap-label' value={1230}>
-								<span className='gap-text'> 20:30 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap1260'
-								name='gap'
-								value={1260}
-								itemProp='0'
-							/>
-							<label htmlFor='gap1260' className='time-gap-label' value={1260}>
-								<span className='gap-text'> 21:00 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap1320'
-								name='gap'
-								value={1320}
-								itemProp='0'
-							/>
-							<label htmlFor='gap1320' className='time-gap-label' value={1320}>
-								<span className='gap-text'> 22:00 </span>
-							</label>
-						</div>
-						<div className='gap-wrapper'>
-							<input
-								type='radio'
-								className='timeGap gap'
-								id='gap1380'
-								name='gap'
-								value={1380}
-								itemProp='0'
-							/>
-							<label htmlFor='gap1380' className='time-gap-label' value={1380}>
-								<span className='gap-text'> 23:00 </span>
-							</label>
-						</div>
+						<TimeOption
+							id='gap720'
+							value={'12:00'}
+							label='12:00'
+							onChange={handleChangeFormData}
+							className={selectedTime === '12:00' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap750'
+							value={'12:30'}
+							label='12:30'
+							onChange={handleChangeFormData}
+							className={selectedTime === '12:30' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap780'
+							value={'13:00'}
+							label='13:00'
+							onChange={handleChangeFormData}
+							className={selectedTime === '13:00' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap810'
+							value={'13:30'}
+							label='13:30'
+							onChange={handleChangeFormData}
+							className={selectedTime === '13:30' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap840'
+							value={'14:00'}
+							label='14:00'
+							onChange={handleChangeFormData}
+							className={selectedTime === '14:00' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap870'
+							value={'14:30'}
+							label='14:30'
+							onChange={handleChangeFormData}
+							className={selectedTime === '14:30' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap900'
+							value={'15:00'}
+							label='15:00'
+							onChange={handleChangeFormData}
+							className={selectedTime === '15:00' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap930'
+							value={'15:30'}
+							label='15:30'
+							onChange={handleChangeFormData}
+							className={selectedTime === '15:30' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap960'
+							value={'16:00'}
+							label='16:00'
+							onChange={handleChangeFormData}
+							className={selectedTime === '16:00' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap990'
+							value={'16:30'}
+							label='16:30'
+							onChange={handleChangeFormData}
+							className={selectedTime === '16:30' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap1020'
+							value={'17:00'}
+							label='17:00'
+							onChange={handleChangeFormData}
+							className={selectedTime === '17:00' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap1050'
+							value={'17:30'}
+							label='17:30'
+							onChange={handleChangeFormData}
+							className={selectedTime === '17:30' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap1080'
+							value={'18:00'}
+							label='18:00'
+							onChange={handleChangeFormData}
+							className={selectedTime === '18:00' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap1110'
+							value={'18:30'}
+							label='18:30'
+							onChange={handleChangeFormData}
+							className={selectedTime === '18:30' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap1140'
+							value={'19:00'}
+							label='19:00'
+							onChange={handleChangeFormData}
+							className={selectedTime === '19:00' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap1170'
+							value={'19:30'}
+							label='19:30'
+							onChange={handleChangeFormData}
+							className={selectedTime === '19:30' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap1200'
+							value={'20:00'}
+							label='20:00'
+							onChange={handleChangeFormData}
+							className={selectedTime === '20:00' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap1230'
+							value={'20:30'}
+							label='20:30'
+							onChange={handleChangeFormData}
+							className={selectedTime === '20:30' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap1260'
+							value={'21:00'}
+							label='21:00'
+							onChange={handleChangeFormData}
+							className={selectedTime === '21:00' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap1320'
+							value={'22:00'}
+							label='22:00'
+							onChange={handleChangeFormData}
+							className={selectedTime === '22:00' ? 'selected' : ''}
+						/>
+						<TimeOption
+							id='gap1380'
+							value={'23:00'}
+							label='23:00'
+							onChange={handleChangeFormData}
+							className={selectedTime === '23:00' ? 'selected' : ''}
+						/>
 					</div>
 				</div>
 				<div className='form-date__comment'>
 					<input
 						type='text'
 						className='form-date__comment-input'
+						value={formData.сomment}
 						id='comment'
+						onChange={handleChangeFormData}
 						maxLength={180}
-						name='Comment'
+						name='сomment'
 						placeholder='Пожелание к бронированию'
 					/>
 				</div>
 				<p className='form-date__info'>
-					Нажимая кнопку «Продолжить» вы принимаете условия
+					Нажимая кнопку «Забронировать» вы принимаете условия
 					<span> пользовательского соглашения</span> ,
 					<span> политики конфиденциальности</span>
 				</p>
 			</form>
 			<footer className='modal-footer flex'>
 				<div></div>
-				<button type='button' className='modal-footer__btn' disabled>Забронировать</button>
+				<button
+					type='submit'
+					form='reservationForm'
+					className='modal-footer__btn'
+					disabled={!formData.gap}
+				>
+					Забронировать
+				</button>
 			</footer>
 		</dialog>
 	);
